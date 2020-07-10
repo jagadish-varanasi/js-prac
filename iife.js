83cef146f6bc9e67223850bb44931624524c3b5c
Jagadish Babu V <44795436+vjagadishvaranasi@users.noreply.github.com>
2020-07-10 09:12:24 +0530

    Update iife.js

--- iife.js
+++ iife.js
@@ -11,3 +11,18 @@
   var score=Math.random()*10-good;
   console.log(score);
 })(1);
+
+
+
+///closures
+
+
+function clo(app){
+  var a="Jaga";
+  return function(add1){
+    console.log("Your num "+add1+" and global values are "+" "+app+" "+a);
+    console.log("Even we can acess add1 and app value bcz of scope chain! and this is call closures where values are access even after function returns")
+  }
+}
+
+clo('Hello')(12);
